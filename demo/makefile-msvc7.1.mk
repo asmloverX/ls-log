b3ae9217d6822611c22fd7ac8fd6298c5a096a57
ASMlover <asmlover@126.com>
2014-03-16 18:58:06 +0800

    fixed display formatting for ls-log

--- demo/makefile-msvc7.1.mk
+++ demo/makefile-msvc7.1.mk
@@ -1,13 +1,14 @@
-BIN		= ls-demo.exe
-CC 		= cl -c -nologo
-CFLAG	= -O2 -W4 -MD -GS -Zi -Fd"vc.pdb" -DNDEBUG -D_CRT_SECURE_NO_DEPRECATE\
-				-D_CRT_NONSTDC_NO_WARNINGS -DLS_LOG_ON -I"../inc"
+BIN	= ls-demo.exe
+CC	= cl -c -nologo
+CFLAG	= -O2 -W4 -MD -GS -Zi -Fd"vc.pdb" -DNDEBUG\
+	-D_CRT_SECURE_NO_DEPRECATE -D_CRT_NONSTDC_NO_WARNINGS\
+	-DLS_LOG_ON -I"../inc"
 LINK	= link -nologo
-LFLAG	= -DEBUG -PDB:"ls-demo.pdb" kernel32.lib user32.lib gdi32.lib \
-				-NODEFAULTLIB:"libc.lib" -NODEFAULTLIB:"libcmt.lib"
-RM 		= rm
-SRCS 	= main.c
-OBJS 	= main.obj
+LFLAG	= -DEBUG -PDB:"ls-demo.pdb" kernel32.lib user32.lib gdi32.lib\
+	-NODEFAULTLIB:"libc.lib" -NODEFAULTLIB:"libcmt.lib"
+RM	= rm
+SRCS	= main.c
+OBJS	= main.obj
 
 $(BIN):$(OBJS)
 	$(LINK) -out:$(BIN) $(OBJS) $(LFLAG)
